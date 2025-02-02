28ce1c1307a0aa69d91c775ed8b092b9235bc358
Alan <alan.gio@gmail.com>
2025-02-01 21:31:21 -0300

    Create node backend api

---
+++ build/server/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import compression from "compression";
+const server = express();
+import router from "../config/routes.js";
+router.use(compression());
+router.use(cors());
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+server.use("/", router);
+export default server;
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import "dotenv/config";
+import { MongoDBSocketClient } from "./db/mongo_db_server_client.js";
+
+const client = await MongoDBSocketClient.getInstance();
+import server from "./server/server.js";
+
+server.listen(3000, function () {
+  console.log("Server running in Port 3000!");
+});
